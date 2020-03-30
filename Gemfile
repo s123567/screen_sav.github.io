9a3dd3ac36a302f67779b6ae8e9b03ca3139ca33
Mathieu Chiron <mathieu@deepidoo.com>
2020-03-30 12:13:29 +0200

    init duplicate from copy of screen sav

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-sprockets'
+gem 'middleman-deploy', git: 'https://github.com/lewagon/middleman-deploy.git'
+gem 'pry-byebug', require: false
+gem 'sass'
+gem "nokogiri", ">= 1.10.4"
+
+# Fix critical github warning
+gem 'actionview', '>= 5.0.7.2'
+
+# Assets
+gem 'bootstrap', '~> 4.3.1'
+gem 'jquery-middleman'
---
+++ config.rb
@@ -0,0 +1,52 @@
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+activate :sprockets
+
+
+# ------ Uncomment and adapt if you want to use i18n ------
+# activate :i18n, mount_at_root: :fr, langs: [:fr, :en]
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+activate :i18n,
+   :mount_at_root => 'fr',
+   :path => "/:locale/"
+
+configure :build do
+  activate :minify_css
+  # ------ We commented this command as it prevented us to deploy our Middleman project on GitHub Pages ------
+  # activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+  set :relative_links, true
+end
+
+
+helpers do
+  def local_img
+    if I18n.locale == :fr
+      return :img_fr
+    else
+      return :img_en
+    end
+  end
+
+  def current_local
+    if I18n.locale == :fr
+      return 'fr'
+    else
+      return 'en'
+    end
+  end
+end
+
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+  deploy.branch = "master"
+  deploy.remote = "git@github.com:s123567/s123567.github.io.git"
+end
